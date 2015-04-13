7d9986164bc64f75156ce5660be9d71055b8b30d
pkinausleap <paul.kimber@leap.com.au>
2015-04-13 03:21:43 +0000

    # Pd                                                   lease enter the commit message for your changes.     sdfsdfsdfsdfdsf     wp     :q     :Q     :q     # Please enter the commit message for your changes.

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -6,3 +6,24 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+#
+# check-repo\cookbooks\apache\reciepies\default.#!/usr/bin/env ruby -wKU
+
+#install apache
+
+#start the apache service
+#make sure the service start on reboot
+
+#write our home page
+
+package "apache2"  do
+	action :install
+end
+
+service "apache2" do
+	action [:start, :enable]
+end
+
+
+
+
--- cookbooks/learn_chef_iis/recipes/default.rb
+++ cookbooks/learn_chef_iis/recipes/default.rb
@@ -28,3 +28,8 @@ template 'c:\inetpub\wwwroot\Other1.htm' do
 	source 'index.html.erb'
 end
 
+windows_package '7-Zip 9.20 (x64 edition)' do
+  source 'http://downloads.sourceforge.net/sevenzip/7z920-x64.msi'
+  action :install
+end
+
